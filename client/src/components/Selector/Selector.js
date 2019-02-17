ecf06903706e44bf9c4db6676e5bd506f62039d0
Andy Wilk <andrewdwilk@gmail.com>
2019-02-17 17:33:06 -0500

    pulling members of sets, but id one behind"

--- client/src/components/Selector/Selector.js
+++ client/src/components/Selector/Selector.js
@@ -7,9 +7,10 @@ const Selector = (props) => {
         <label for="exampleFormControlSelect1">Example select</label>
         <select class="form-control" id="exampleFormControlSelect1" onChange={props.function}>
           <option >Select Course</option>
-          <option id="1327214400000521">Law 101</option>
+          <option id="1327214440000521">Law 101</option>
+
           <option id="687343570000521">Law 222</option>
-          <option id="1325705530000521">Law 333</option>
+          <option id="1327214350000521">Law 333</option>
         </select>
 
       </div>
